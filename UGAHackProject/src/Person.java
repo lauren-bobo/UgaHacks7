af149448ca772c83467334894f99e9966ac895b2
Dominarch <100043382+Dominarch@users.noreply.github.com>
2022-02-19 17:11:39 -0500

    hell

--- UGAHackProject/src/Person.java
+++ UGAHackProject/src/Person.java
@@ -1,4 +1,19 @@
+/**
+ * 
+ * @author Dominarch
+ *
+ */
 
 public class Person {
-
+	public enum  {
+		Maven, Connector, Salesperson
+		} 
+	private double x;
+	private double y; 
+	
+	
+public Person (double initialX, double initialY) {
+	x = initialX; 
+	y = initialY;
+}
 }
