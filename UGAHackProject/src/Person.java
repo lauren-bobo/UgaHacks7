71e1fa7bd315e3510d47879d3a9fa915ce936060
Dominarch <100043382+Dominarch@users.noreply.github.com>
2022-02-19 17:17:36 -0500

    Update Person.java

--- UGAHackProject/src/Person.java
+++ UGAHackProject/src/Person.java
@@ -1,19 +1,20 @@
-/**
- * 
- * @author Dominarch
- *
- */
 
 public class Person {
-	public enum  {
-		Maven, Connector, Salesperson
-		} 
-	private double x;
-	private double y; 
+
+		private double x;
+		private double y; 
+		
+		
+	public Person(double initialX, double initialY) {
+		x = initialX; 
+		y = initialY;
+	}
 	
+	public void setX double newX) {
+		x = newX
+	}
 	
-public Person (double initialX, double initialY) {
-	x = initialX; 
-	y = initialY;
-}
-}
+	public void setX(double newY) {
+		x = newX
+	}
+	}
