9b535aaee25bb695f48ee63f6b326d0f016196be
Dominarch <100043382+Dominarch@users.noreply.github.com>
2022-02-20 05:36:04 -0500

    Update StringReference.java

--- UGAHackProject/src/StringReference.java
+++ UGAHackProject/src/StringReference.java
@@ -1,4 +1,119 @@
 
 public class StringReference {
 
+public String level1(int n) {
+	String[] s = {
+			"CDC announces new health policy against Covid: Hold classes in outside pavillions.",
+			"Honey bee farms are now illegal in the following states: Washington, Wyoming, Wisconson.",
+			"A Renaissance festival in Texas goes arry after people dressed as \"early concepts of witches\" freaks out a handful of elderly couples.",
+			"Renown pop star and song artist Taylor Swift begins holding raves after discovering her love for EDM.",
+			"Value of beauty products skyrocket after a subreddit known as \"r/MakeupAddiction\" sends out it's community to raid beauty product stores all over the country.",
+			"Loyal Samsung customers outraged after yet another samsung pen is released with a different name, the same structure and an even crazier price.",
+			
+	} ;
+	return s[n];
+}
+
+public String level2(int n) {
+	String [] s = {
+			"A local zoo in southeast Nebraska builds a dome around the property to let smaller creatures run free and interact with Patrons. Airborne wildife causes ruckus at concession stands.",
+			"Heartsfield Jackson international airport in Atlanta, GA opens yet another Bojangles in concourse E. Facility designers and contractors questioned.",
+			"NASA releases footage of the rings of Saturn. Abundance of crystalized Oil shale and fossil fuel has United States investing in space travel more than ever before.",
+			"Diehard fans of popular game \"Dungeons and Dragons\" express excitement about the \"addition of 10 new races\" in the newest upcoming release.",
+			"Motorcycle technology advancing faster than ever before. Leading manufacturers boast about \"Akira-level bike quality\" in their upcoming products.", 
+			"People living in the swamps of southern Florida evacuate their homes after a sudden and dreadful increase in \"Yellow Feaver\" cases.",
+			"Excusive from \"The Red and Black\" : UGA Hacks NFTS plummet in value. Computer Scientists all over campus outraged. "
+	};
+	return s[n];
+}
+
+public String level3(int n) {
+	String [] s = {
+			"Germany confirmed to be the first country to reach the moon after brutalizing evidence is presented. Popular German Rock band \"Rammstein\" Releases song mocking their moonfaring rivals \"In Amerika\".",
+			"Tiktok trend of high schoolers interlocking braids causes lice outbreaks all over coutries. Parents begin first wave of assult on Tiktok HQ with hundereds of lawsuits.",
+			"Popular music streaming service \"Soundcloud\" finally gets it's own desktop app after ten years of development.",
+			"A sudden increase in the population of Buffalo in Colorado causes increase in buffalo-related break-ins.",
+			"UGA Black Exclusive: NCR CTO Kim Vanderham exposed and cancelled for not actually eating hot wings on \"Hot Ones\" interview for UGA Hacks. NCR company value begins to plummet. ",
+			"Scientists at NASA confirm higher probability of being hit by sattelites revolving around earth. Surveys show steady increase in interest for all space-faring careers.",
+			"Scientists come closer to confirming the existance of the paranormal after connecting the location of Death Metal concerts to sudden spike in electromagnetic energy in nearby graveyards."
+			
+	} ;
+	return s[n];
+}
+
+public String level4(int n) {
+	String [] s = {
+			"Trend Alert: Learning how to juggle your newborn children becomes the hottest new trend amongst Facebook dads." ,
+			"A serial killer brought in for questioning admits that having a violin for their weapon of choice is due to their love of soap operas.",
+			"Members of the subreddit \"r/AliensAmongUs\" pinned responsible for building monoliths made out of cardboard all over the deserts of Phoenix, Arizona.",
+			"Dying mobile service company \"Mint Mobile\" makes suprising profit off of the elderly by selling them landline phone systems.",
+			"Trend Alert: Recent trend in heavy metal bands breaking acoustic guitars on stage creates intense rivalry between heavy metal lovers and alt rock lovers.",
+			"Newest breed in dog named \"The werewolf\" after observations show that a fully grown adult is able to stand on it's hind legs longer than any other breed of dog.",
+			"UGA Red and Black Exclusive: MLC Learning center and Ramsey Physical Student Center switch facilities after an increase in students bringing gym equipment to MLC, specifically pull-up bars."
+			
+	} ;
+	return s[n];
+}
+
+public String level5(int n) {
+	String [] s = {
+			"Orlando Resort forced to shut down after a massive amount of fake blood leaks onto the disewalks from their latest attraction based on \"The Shining\"." ,
+			"Producers of the movie \"Cats\" plan on recasting for a play with human-cat mutants as shown in the latest film adaptation." ,
+			"Kitchenware companies in Russia refuse to stop painting their products with uranium-infused paint. Russian scientists come closer to predicting the date of a \"second chernobyl\".",
+			"Trend Alert: A certain throwback thursday post on facebook has people hyping up another raid on Area 51. Fans of Japanese animation everywhere begin their preperations." ,
+			"The unknown culprit of the incident involving suggestive content from the game \"Final Fantasy 7\" repeats his acts of tyranny at the national conference in France." ,
+			"Dozens of youtube animators ditch patreon after tumblr lifts it's ban on suggestive content across their platform. Youtube's net value begins to show signs of decreasing.",
+			"UGA Red and Black Exclusive: Hundereds of students violently protest in the streets after \"Chocolate Pie\" is removed from dining halls all across campus."
+
+			
+	} ;
+	return s[n];
+}
+public String level6(int n) {
+	String [] s = {
+			"Local town in Missisippi gets the wrong idea of \"National Women's day\" by crossdressing and acting in a sexist mannor. Major backlash was thrown at them on all platforms of social media.",
+			"Trend Alert: A sudden spike in people wearing black clothing and acessories due to a trend on Instagram activity induces a second \"Witch Hunt\" in the town of Salem, Massacusets. Local policed forced to close down multiple churches from spreading misinformation.",
+			"An EDM festival in Denver, Colorado causes a devastating 0.1% increase in radius of the hole in the ozone layer due to the amount of marijuana used being the most since Woodstock."
+	} ;
+	return s[n];
+}
+
+public String level7(int n) {
+	String [] s = {
+			"Trend Alert: A new tik tok trend is causing people to raid Papa John's pizza stores with Domino's pizza boxes strapped to their bodies with duct tape. Court cases dealing with manslaughter and aggrivated assult charges have been at an all time high.",
+			"Trend Alert: Dads on Facebook are racking up likes by replicating car accidents seen in videos posted on \"r/IdiotsInCars\". This trend has a high probability of spreading to Tik Tok users according to multiple surveys.",
+			"UGA Red and Black Exclusive: A rabies outbreak at UGA has caused all the service dogs to turn feral and terrorize students on their way to class."
+			 
+	} ;
+	return s[n];
+}
+public String level8(int n) {
+	String [] s = {
+			"Speculation about the U.S government forming an alliance with an alien race is at an all time high after dozens of police reports of cows being \"abducted by UFOS\" in farm lands all over the country.",
+			"Japan exposed for building secret laboratories inside of the volcanoes of Hawaii after a few dozen police reports of local residents being attacked by \"mutant\" marine wildlife on shores close to Hawaiian volcanoes.",
+			"UGA Red and Black Exclusive: MLC turned into a heavily armored base after participants of UGA hacks storm and capture MLC with highly advanced nerf guns."
+	
+	} ;
+	return s[n];
+}
+public String level9(int n) {
+	String [] s = {
+			"Trend Alert: Ouiji board trend on Tik Tok peaking around Halloween spirals out of control when police stations recieve a massive increase in calls relating to \"possesion\" and \"poltergeist activity\".",
+			"Grizzly Bears all over the country have been spotted \"walking on two legs and scaring away campers with sticks and clubs to steal their food\". Scientists speculate that these bears have \"lost all fear of humans\".",
+			"UGA Red and Black Exclusive: Representatives of earth's underground reptilian race visit UGA to give talks and presentations about their every day lives underground and in the shadows."
+	} ;
+	return s[n];
+}
+
+public String level10(int n) {
+	String [] s = {
+			"U.S military's scientific department in deep trouble after letting dozens of revived \"dinosaurs\" break loose from their secret laboratory in Golden, Colorado and wreak havoc in nearby towns.",
+			"Trend Alert: A new Tik Tok trend takes a turn for the worst when people begin investing in bionic limbs to have attached to them after paying to have their limbs amputated. Tik Tokers claim to be imitating characters from the hit game \"Cyberpunk 2077\".",
+			"UGA Red and Black Exclusive: Super AI created for a UGA Hacks project breaks loose from local stoarge and begins crashing entire networks students use to register for classes. Creators of super AI claim that it is possible for the AI to access private data if connected to the campus - wide wifi." 
+	};
+			return s[n];
+}
+
+
+
 }
